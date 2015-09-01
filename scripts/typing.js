3075c6684f7e248d57c809835214cfe7e7f731e6
Christine Cha <hello@christinecha.com>
2015-09-01 19:10:06 -0400

    Updates links.

---
+++ scripts/typing.js
@@ -0,0 +1,9 @@
+
+<script>
+    $(function(){
+        $(".typedphrase").typed({
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 0
+        });
+    });
+</script>
