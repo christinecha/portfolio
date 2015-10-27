7ac304bc86f129a62671f278bab7ff0ce1ea1abd
Christine Cha <hello@christinecha.com>
2015-10-27 14:13:48 -0400

    New look.

--- scripts/typing.js
+++
@@ -1,9 +0,0 @@
-
-<script>
-    $(function(){
-        $(".typedphrase").typed({
-            strings: ["First sentence.", "Second sentence."],
-            typeSpeed: 0
-        });
-    });
-</script>
