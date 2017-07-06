720666760f95b29a7564e82ce76692db4e236fa6
Christine Cha <ccha@squarespace.com>
2017-07-05 22:44:19 -0400

    Updated hover effect

---
+++ src/scripts/Jumbler.js
@@ -0,0 +1,65 @@
+class Jumbler {
+  constructor($element) {
+    this.$element = $element
+
+    this.makeSpans()
+
+    this.$chars = Array.prototype.slice.call(this.$element.getElementsByClassName('char'))
+  }
+
+  makeSpans() {
+    const innerHTML = this.$element.innerHTML.split('              ').join('')
+    const $children = Array.prototype.slice.call(this.$element.children)
+
+    let _innerHTML = innerHTML
+
+    const childrenMap = {}
+
+    $children.forEach($child => {
+      const i = innerHTML.indexOf($child.outerHTML)
+      childrenMap[i] = $child.outerHTML
+      _innerHTML = _innerHTML.split($child.outerHTML).join(i)
+    })
+
+    let _newInnerHTML = ''
+
+    const words = _innerHTML.split(' ')
+
+    words.forEach(word => {
+      let _newWordHTML = '<span>'
+
+        const chars = word.split('')
+
+        if (childrenMap[word]) {
+          console.log('hi', childrenMap[word], _newWordHTML)
+          _newWordHTML += '<span class="char">' + childrenMap[word] + '</span>'
+        } else {
+          chars.forEach(char => {
+            _newWordHTML += '<span class="char">' + char + '</span>'
+          })
+        }
+
+      _newWordHTML += '</span>'
+      _newInnerHTML += _newWordHTML + '<span class="space"> </span>'
+    })
+
+    this.$element.innerHTML = _newInnerHTML
+  }
+
+  jumble() {
+    this.$chars.forEach($char => {
+      const x = Math.random() * 40 - 20
+      const y = Math.random() * 40 - 20
+      const z = Math.random() * 20 - 10
+      $char.style.transform = `translate3d(${x}px,${y}px,0) rotateZ(${z}deg)`
+    })
+  }
+
+  unjumble() {
+    this.$chars.forEach($char => {
+      $char.style.removeProperty('transform')
+    })
+  }
+}
+
+module.exports = Jumbler
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,47 @@
-const html = document.getElementById('portfolio')
-console.log(html, document)
+const Jumbler = require('./Jumbler')
 
-console.log("Hey, bozo.")
+const $projects = Array.prototype.slice.call(document.getElementsByClassName('project'))
+
+let $activeProject = null
+
+const handleMouseEnter = (e) => {
+  $activeProject = e.srcElement
+  updateView()
+}
+
+const handleMouseLeave = (e) => {
+  if ($activeProject === e.srcElement) $activeProject = null
+  updateView()
+}
+
+const updateView = () => {
+  projects.forEach(project => {
+    const isActive = $activeProject === project.$el
+
+    if (!$activeProject || isActive) {
+      project.$el.classList.add('is-active')
+      project.title.unjumble()
+      project.body.unjumble()
+    } else {
+      project.$el.classList.remove('is-active')
+      project.title.jumble()
+      project.body.jumble()
+    }
+  })
+}
+
+const projects = $projects.map($project => {
+  $project.addEventListener('mouseenter', handleMouseEnter)
+  $project.addEventListener('mouseleave', handleMouseLeave)
+
+  const $title = $project.getElementsByTagName('A')[0]
+  const $body = $project.getElementsByTagName('P')[0]
+
+  return {
+    $el: $project,
+    title: new Jumbler($title),
+    body: new Jumbler($body)
+  }
+})
+
+updateView()
